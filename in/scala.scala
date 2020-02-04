a580a8a75fa215e8eb6041cfdcf23dc2bb6660c7
Sammwy <44925968+sammwyy@users.noreply.github.com>
2020-02-04 00:28:30 -0300

    Update and rename scala.scala to in/scala.scala

---
+++ in/scala.scala
@@ -0,0 +1,6 @@
+object Example {
+  def main(args: Array[String]): Unit = {
+    // Hello world in Scala
+    println("Hello world")
+  }
+}
--- scala.scala
+++
@@ -1,6 +0,0 @@
-object Example {
-  	def main(args: Array[String]): Unit = {
-  		// Hello world in Scala
-    	println("Hello world")
-  	}
-}
